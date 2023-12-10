f24435cb213bb58800a4b6720115ead750473515
Himanshu0917 <132185307+Himanshu0917@users.noreply.github.com>
2023-12-10 10:07:03 +0000

    Two pointer

---
+++ Arrays and Strings/Reverse_Vowels_Of_A_String.java
@@ -0,0 +1,46 @@
+class Solution {
+    public String reverseVowels(String s) {
+        char arr[] = s.toCharArray();
+        int i=0;
+        int j = s.length()-1;
+        while(i<j){
+            if(isVowel(arr[i]) && isVowel(arr[j]))
+            {
+                char ch = arr[i];
+                arr[i] = arr[j];
+                arr[j] = ch;
+                i++;
+                j--;
+            }
+           else if(isVowel(arr[i]))
+            {
+                j--;
+            }
+          else  if(isVowel(arr[j]))
+            {
+                i++;
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return new String(arr);
+    }
+
+    boolean isVowel(char ch){
+        if(ch == 'a' || ch=='A')
+        return true;
+        else if(ch == 'e' || ch=='E')
+        return true;
+         else if(ch == 'i' || ch=='I')
+        return true;
+         else if(ch == 'o' || ch=='O')
+        return true;
+         else if(ch == 'u' || ch=='U')
+        return true;
+        else
+        return false;
+
+    }
+}
